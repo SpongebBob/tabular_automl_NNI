022260b3ba53466022f431b0964f24d1b320646f
SpongebBob <674785731@qq.com>
2019-07-29 21:41:42 +0800

    add test main and yaml

---
+++ test_config/cancer/main.py
@@ -0,0 +1,62 @@
+# Copyright (c) Microsoft Corporation
+# All rights reserved.
+#
+# MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+# to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import nni
+import logging
+import numpy as np
+import pandas as pd
+import json
+import sys
+from sklearn.preprocessing import LabelEncoder
+
+sys.path.append('../../')
+
+from fe_util import *
+from model import *
+
+logger = logging.getLogger('auto-fe-examples')
+
+if __name__ == '__main__':
+    file_name = ' ~/Downloads/breast-cancer.data'
+    target_name = 'Class'
+    id_index = 'Id'
+
+    # get parameters from tuner
+    RECEIVED_PARAMS = nni.get_next_parameter()
+    logger.info("Received params:\n", RECEIVED_PARAMS)
+    
+    # list is a column_name generate from tuner
+    df = pd.read_csv(file_name, sep = ',')
+    df.columns = [
+        'Class', 'age', 'menopause', 'tumor-size', 'inv-nodes',
+        'node-caps', 'deg-malig', 'breast', 'breast-quad', 'irradiat'
+    ]
+    df['Class'] = LabelEncoder().fit_transform(df['Class'])
+    
+    if 'sample_feature' in RECEIVED_PARAMS.keys():
+        sample_col = RECEIVED_PARAMS['sample_feature']
+    else:
+        sample_col = []
+    
+    # raw feaure + sample_feature
+    df = name2feature(df, sample_col, target_name)
+    feature_imp, val_score = lgb_model_train(df,  _epoch = 1000, target_name = target_name, id_index = id_index)
+    nni.report_final_result({
+        "default":val_score, 
+        "feature_importance":feature_imp
+    })
+
